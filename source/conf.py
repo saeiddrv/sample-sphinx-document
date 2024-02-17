b24766051a88f7df3b2bd240306831d9e4c92a64
Saeid Darvish <saeid.drgh@gmail.com>
2024-02-18 00:27:44 +0100

    set sphinx_rtd_theme theme

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'OpenSourceDocument'
+project = 'Sample-Sphinx-Document'
 copyright = '2024, Saeid Darvish'
 author = 'Saeid Darvish'
 release = '0.0.1'
@@ -14,7 +14,11 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+]
+
+
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +28,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
